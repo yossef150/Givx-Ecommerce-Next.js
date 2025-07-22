0187f23184db802e571c1ecb565e4b348812691f
yossef150 <yossefmedhat432@gmail.com>
2025-07-22 18:29:08 +0300

    updated the logo

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { APP_NAME } from '@/lib/constants'
-import Image from 'next/image'
 import Link from 'next/link'
 import Menu from './Menu'
 function Index() {
@@ -8,10 +7,13 @@ function Index() {
     <header className='border-b'>
       <div className="wrapper flex-between">
         <div className="flex-start space-x-2">
-            <Link href= '/'>
-                <Image src= '/images/logo.svg' alt={`${APP_NAME} logo`} width={48} height={48}></Image>
+            <Link href= '/' className="flex-start space-x-2">
+              <span className="text-3xl font-extrabold tracking-wide text-black dark:text-white transition-transform duration-300 hover:scale-105">
+                {APP_NAME}
+              </span>
+                {/* <Image src= '/images/logo.svg' alt={`${APP_NAME} logo`} width={48} height={48}></Image> */}
+            {/* <span className='hidden lg:block font-bold text-3xl'>{APP_NAME}</span> */}
             </Link>
-            <span className='hidden lg:block font-bold text-2xl'>{APP_NAME}</span>
         </div>
         <Menu/>
       </div>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = process.env.PUBLIC_APP_NAME || 'Ukanda';
-export const APP_DESCRIPTION = process.env.PUBLIC_APP_DESCRIPTION || 'Ukanda';
+export const APP_NAME = process.env.PUBLIC_APP_NAME || 'GivX';
+export const APP_DESCRIPTION = process.env.PUBLIC_APP_DESCRIPTION || 'GivX';
 export const APP_SERVER_URL = process.env.PUBLIC_APP_SERVER_URL || 'http://localhost:3000/';
 export const LATEST_COLLECTION_LIMIT = process.env.LATEST_COLLECTION_LIMIT || 4;
